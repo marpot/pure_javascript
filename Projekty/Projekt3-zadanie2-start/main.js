8dc061bebd8e9f13d4295d97b4aa0e4fea793921
Marcin Potoczny <marcin.potoczny@protonmail.com>
2024-06-24 16:55:38 +0200

    Updated projects

--- Projekty/Projekt3-zadanie2-start/main.js
+++ Projekty/Projekt3-zadanie2-start/main.js
@@ -2,11 +2,34 @@ let size = 10;
 let orderElement = 1;
 
 const init = () => {
- // tutaj kod
+    const button = document.createElement('button');
+    const buttonReset = document.createElement('button');
+    button.innerText = "Dodaj 10 elementów listy";
+    buttonReset.innerText = "Wyczyść";
+    const list = document.createElement('ul');
+    document.body.appendChild(button);
+    document.body.appendChild(buttonReset);
+    document.body.appendChild(list);
+
+    button.addEventListener("click", createLiElements);
+    buttonReset.addEventListener("click", cleanList);
 }
 
 const createLiElements = () => {
- // tutaj kod
+    const list = document.querySelector('ul');
+    for (let i = 0; i<10; i++){
+        const li_element = document.createElement("li");
+        li_element.innerText = `Element ${orderElement++}`;
+        li_element.style.fontSize = `${size++}px`;
+        list.appendChild(li_element);
+    }
+}
+
+const cleanList = () => {
+    const list = document.querySelector('ul');
+    list.innerHTML = "";
+    size = 10;
+    orderElement = 1;
 }
 
 init()
